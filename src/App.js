833b4f522a641a31527ca864d25e9aeffa21b103
Enoch <mmejuenoch@gmail.com>
2022-11-03 23:52:47 +0100

    I created the Nav.js component in the navComponent folder and added all necessary contents to it. I styled it's contents in index.css. I then linked Nav.js to the App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./navComponent/Nav.js"
 
 const App = () => {
   return (
-    <div className="App">
-      working
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+      </div>
+    </Router>
   )
 }
 

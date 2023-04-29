9f31ede5fc8e9132fa519928f650b14d036edcf2
Enoch <mmejuenoch@gmail.com>
2023-04-29 21:57:23 +0100

    Setup for next, typescript and tailwindcss in project is done. Moving on to the development of my portfolio site.

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="h-screen bg-gray-800 text-white">
+      <h1>Home Page</h1>
+      <p>This is the home page</p>
+    </div>
+  );
+}
